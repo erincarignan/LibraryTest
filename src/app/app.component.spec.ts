e33ed40753645ee2e58b931ac5766c9613f23329
Erin Carignan <erin_carignan@waters.com>
2018-09-28 11:54:37 -0400

    Angular lib with wallaby failure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TestlibModule } from 'testlib';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [TestlibModule],
       declarations: [
         AppComponent
       ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestlibModule } from 'testlib';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, TestlibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
